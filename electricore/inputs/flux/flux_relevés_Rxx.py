9a7714ccb103bd6e8e462311e093b5d6d9e5c40d
virgile <virgile.dauge@pm.me>
2025-02-21 17:48:32 +0100

    filtrage des données indonnues et DN999999

--- electricore/inputs/flux/flux_relevés_Rxx.py
+++ electricore/inputs/flux/flux_relevés_Rxx.py
@@ -12,6 +12,12 @@ def charger_flux_r151(source: pd.DataFrame) -> DataFrame[RelevéIndex]:
     df: DataFrame[FluxR151] = FluxR151.validate(source)
     df["Source"] = "flux_R151"
 
+    # Supprimer des lignes ou 'Id_Calendrier_Distributeur' == 'INCONNU'
+    # Dans la doc enedis, c'est quand il n'y a pas de mesure. Pourquoi en faire une entrée alors ? Yo no sé
+    # Supprimer les lignes où 'Id_Calendrier_Distributeur' == 'INCONNU' et == 'DN999999'
+    # Note : Je ne sais pas ce que DN999999 vient faire là. C'est dans ces lignes qu'on retrouve des valeurs dans la colonnes INCONNU. Pour l'instant on supprime.   
+    df = df[~df['Id_Calendrier_Distributeur'].isin(['INCONNU', 'DN999999'])]
+
     # Réordonner les colonnes pour correspondre au modèle Pandera
     ordre_colonnes = FluxR151.to_schema().columns.keys()
     df = df[ordre_colonnes]
@@ -19,4 +25,5 @@ def charger_flux_r151(source: pd.DataFrame) -> DataFrame[RelevéIndex]:
     # Supprimer des colonnes si présentes
     _to_drop: list[str] = [c for c in ['INCONNU'] if c in df.columns]
     df = df.drop(columns=_to_drop)
+
     return RelevéIndex.validate(df)
