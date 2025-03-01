0583019a430d8f6e428c280c8ab8f9f9851c5899
virgile <virgile.dauge@pm.me>
2025-03-01 15:28:48 +0100

    ajout des modèles

--- electricore/inputs/flux/__init__.py
+++ electricore/inputs/flux/__init__.py
@@ -1,2 +1,3 @@
+from electricore.inputs.flux.modèles import FluxR151, FluxC15
 from electricore.inputs.flux.flux_relevés_Rxx import lire_flux_r151
 from electricore.inputs.flux.flux_contrats_Cxx import lire_flux_c15
