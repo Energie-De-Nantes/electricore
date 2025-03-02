557df3f0510c943d33d6dccf4cf3639ef88d29b4
virgile <virgile.dauge@pm.me>
2025-03-02 14:05:56 +0100

    séparation en services différents en fonciton de l'input

--- electricore/core/services.py
+++ electricore/core/services.py
@@ -19,15 +19,24 @@ def energies_et_taxes(deb: pd.Timestamp, fin: pd.Timestamp, base: pd.DataFrame) 
         fusion_des_sous_periode(turpe)))
     return final.round(2)
 
+import pandera as pa
+from pandera.typing import DataFrame
 
 from electricore.inputs.flux import (
     FluxC15, FluxR151, 
     lire_flux_c15, lire_flux_r151
 )
-from electricore.core.énergies.fonctions import préparer_base_énergies, ajouter_relevés
+from electricore.core.périmètre import (
+    HistoriquePérimètre, 
+
+)
+from electricore.core.relevés import (
+    RelevéIndex, 
+)
+from electricore.core.énergies.fonctions import préparer_base_énergies, ajouter_relevés, calculer_energies
 
 # TODO rename facturation depuis flux ou un truc du genre. 
-def facturation(deb: pd.Timestamp, fin: pd.Timestamp, c15: pd.DataFrame, r151: pd.DataFrame) -> pd.DataFrame:
+def facturation_flux(deb: pd.Timestamp, fin: pd.Timestamp, c15: pd.DataFrame, r151: pd.DataFrame) -> pd.DataFrame:
     """
     Calcule les énergies et les taxes pour une période donnée, sur l'ensemble du périmètre
     """
@@ -40,3 +49,33 @@ def facturation(deb: pd.Timestamp, fin: pd.Timestamp, c15: pd.DataFrame, r151: p
 
     complet = ajouter_relevés(base, relevés)
     return complet
+
+# TODO rename facturation depuis flux ou un truc du genre.
+@pa.check_types
+def facturation(
+        deb: pd.Timestamp, 
+        fin: pd.Timestamp, 
+        historique: DataFrame[HistoriquePérimètre], 
+        relevés: DataFrame[RelevéIndex]
+        ) -> pd.DataFrame:
+    """
+    Calcule les énergies et les taxes pour une période donnée, sur l'ensemble du périmètre
+    """
+    # Base = pour tous les couples (ref, pdl), on a toutes les Entrées/Sorties du périmètre et le relevés associés
+    base = préparer_base_énergies(historique=historique, deb=deb, fin=fin)
+
+    # TODO: Gestion des cas spécifiques
+
+    # Ajouter les dates de début et de fin pour le cas général (aka les na ici)
+    base["Date_Releve_deb"] = base["Date_Releve_deb"].fillna(deb)
+    base["Date_Releve_fin"] = base["Date_Releve_fin"].fillna(fin)
+
+    # Ajouter les relevés manquants à la base.
+    avec_relevés = base.copy()
+    avec_relevés = ajouter_relevés(avec_relevés, relevés, '_deb')
+    avec_relevés = ajouter_relevés(avec_relevés, relevés, '_fin')
+
+    énergies = calculer_energies(avec_relevés)
+
+    colonnes_triees = sorted(énergies.columns)
+    return énergies.reindex(columns=colonnes_triees)
